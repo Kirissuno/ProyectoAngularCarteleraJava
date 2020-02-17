6e0e3bc1e9e1d9fe8d62107620b09f7278348c54
Robert <arfilip.1h@gmail.com>
2020-02-17 20:00:57 +0100

    v1.0.0
    Falta documentar código.

--- web/src/app/search/search.component.ts
+++ web/src/app/search/search.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Videogame } from '../models/videogame';
 import { VideogameServiceService } from '../services/videogame-service.service';
+import { LoginService } from '../services/login.service';
+import { User } from '../models/user';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -13,12 +17,50 @@ export class SearchComponent implements OnInit {
   noResult : boolean = false;
   videogamesFiltered : Videogame[];
 
+  logedUser : User;
+  isAdmin : boolean = false;
+
+  newGameForm : FormGroup;
+
+  errorNew : boolean = false;
+
   constructor(
     private videogameService : VideogameServiceService,
+    private loginService : LoginService,
+    private modalService : NgbModal,
   ) {
     this.videogames = [];
     this.videogamesFiltered = [];
     this.noResult = false;
+    this.newGameForm = new FormGroup({
+      'title':new FormControl('',  [Validators.required, Validators.minLength(5), Validators.maxLength(255)]),
+      'company':new FormControl('',  [Validators.required, Validators.minLength(5), Validators.maxLength(255)]),
+      'description':new FormControl('',  [Validators.required, Validators.minLength(5), Validators.maxLength(255)]),
+      'imageURL':new FormControl('',  [Validators.required, Validators.minLength(5), Validators.maxLength(255), Validators.pattern('(https?:\/\/.*\.(?:png|jpg))')]),
+      'releaseDate':new FormControl('',  [Validators.required])
+    })
+  }
+
+  onSubmitNew(correcto){
+    console.log(this.newGameForm.value);
+    const newGame :Videogame = {
+      director : this.newGameForm.value.company.trim(),
+      titulo : this.newGameForm.value.title.trim(),
+      descripcion : this.newGameForm.value.description.trim(),
+      urlImage : this.newGameForm.value.imageURL.trim(),
+      fecha : this.newGameForm.value.releaseDate.trim(),
+    }
+    
+    this.videogameService.addGame(newGame).subscribe( () =>{
+      this.modalService.dismissAll();
+      this.ngOnInit();
+      this.modalService.open(correcto);
+    }, () =>{
+      this.errorNew = true;
+      setTimeout(() => {
+        this.errorNew = false;
+      }, 3000);
+    } )
   }
 
   ngOnInit(): void {
@@ -26,7 +68,24 @@ export class SearchComponent implements OnInit {
       this.videogames = games;
     })
     this.noResult = false;
-    this.videogamesFiltered = []
+    this.videogamesFiltered = [];
+    this.logedUser = this.loginService.user;
+    if(this.logedUser != null || this.logedUser.rol != undefined){
+      if(this.logedUser.rol == 'admin'){
+        this.isAdmin = true;
+      }
+    }    
+
+  }
+
+  addGame(modalAdd){
+    this.modalService.open(modalAdd, {size:'md'});
+  }
+
+  deleteGame(title){
+    this.videogameService.deleteGame(title).subscribe(() =>{
+      this.ngOnInit();
+    })
   }
 
   change(){
@@ -47,6 +106,10 @@ export class SearchComponent implements OnInit {
         }
       })
     }    
+  }
+
+  closeModals(){
+    this.modalService.dismissAll();
   }
 
 }
--- web/src/app/services/videogame-service.service.ts
+++ web/src/app/services/videogame-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Videogame } from '../models/videogame';
 
 @Injectable({
   providedIn: 'root'
@@ -27,7 +28,19 @@ export class VideogameServiceService {
     return this.http.get(`${this.URLBase}s/upcomming`);
   }
 
+  addGame(game:Videogame): Observable<any>{
+    return this.http.post(`${this.URLBase}s`, game);
+  }
+
+  modifyGame(title: string, game:Videogame): Observable<any>{
+    return this.http.put(`${this.URLBase}/${title}`, game);
+  }
+
   getLastGames(): Observable<any>{
     return this.http.get(`${this.URLBase}s/last`);
+  }
+
+  deleteGame(title) : Observable<any>{
+    return this.http.delete(`${this.URLBase}/${title}`)
   }
 }
